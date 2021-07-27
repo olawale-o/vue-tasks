e5f9b5871a941b0cbcfa9f94ebe89c7a9f44d72c
olawale-o <omoogunolawale@gmail.com>
2021-07-27 19:28:04 +0100

    Display all tasks

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,10 +1,28 @@
 <template>
-  <ul class="todo-tasks" id="todo-tasks"></ul>
+  <ul class="todo-tasks" id="todo-tasks">
+    <li v-for="task in tasks" :key="task.index" class="todo-list">
+      <div class="field todo-list__task">
+        <label class="label">
+          <input type="checkbox" :checked="task.completed">
+          <span class="checkmark"></span>
+        </label>
+          <span class="todo-list__text">{{task.description}}</span>
+          <i class="bx bx-trash-alt bin hide"></i>
+          <i class="bx bx-dots-vertical-rounded move"></i>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'Todo',
+  props: {
+    tasks: {
+      required: true,
+      type: Array,
+    }
+  },
   setup() {
    
   }
