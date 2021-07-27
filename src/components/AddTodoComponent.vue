5cefc5813deb5a531874a17daee75ad3e996c389
olawale-o <omoogunolawale@gmail.com>
2021-07-27 13:26:34 +0100

    Add AddTodoComponent.vue TodoComponent.vue files

---
+++ src/components/AddTodoComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="input-field">
+    <input type="text" class="input" name="task" id="task" placeholder="Add to your list">
+    <span id="add"><i class='bx bx-subdirectory-left return'></i></span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo',
+    setup() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="todo-tasks" id="todo-tasks"></ul>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  setup() {
+   
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
