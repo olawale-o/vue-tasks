0e312e376cc3c82b7af17ef811245490200ba0c1
olawale-o <omoogunolawale@gmail.com>
2021-07-27 13:36:18 +0100

    Add .gitignore file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Todos />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Todos from './views/Todos.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+</style>
